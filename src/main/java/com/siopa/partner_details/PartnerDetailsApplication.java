59ab04aa9d2de1d51ace14bfe4ce7b3c0d1ad613
Gartland-R2 <Gartland-R2>
2025-02-11 10:44:32 +0000

    first commit

---
+++ src/main/java/com/siopa/partner_details/PartnerDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.siopa.partner_details;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartnerDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartnerDetailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siopa/partner_details/PartnerDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siopa.partner_details;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartnerDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
